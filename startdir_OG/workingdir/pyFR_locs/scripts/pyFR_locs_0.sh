dd6dad54b69494c38dedf9a66bffa3bdaef6ecb9
Jose Carmona <jose87527@gmail.com>
2025-09-11 09:07:35 -0600

    add another startdir to test other things with the pipeline

---
+++ startdir_OG/workingdir/pyFR_locs/scripts/pyFR_locs_0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+# DO NOT MODIFY THIS FILE!
+
+# set the working directory *of the job* to the specified start directory
+cd /home/jose/miniconda3/envs/supereeg_pipeline_test/supereeg_hellgate/startdir/workingdir/scripts
+# run the job
---
+++ startdir_OG/workingdir/pyFR_locs/scripts/pyFR_locs_5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+# DO NOT MODIFY THIS FILE!
+
+# set the working directory *of the job* to the specified start directory
+cd /home/jose/miniconda3/envs/supereeg_pipeline_test/startdir/workingdir/scripts
+# run the job
+python3 /home/jose/miniconda3/envs/supereeg_pipeline_test/startdir/workingdir/scripts/pyFR_locs/union_locs.py hgamma
